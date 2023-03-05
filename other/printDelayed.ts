b3ea7d1a622fb5ae607cc20f0c17c7362408e048
Jeremías Robles <jererobles@users.noreply.github.com>
2023-03-05 23:19:22 +0200

    fixme: too many callbacks

--- other/printDelayed.ts
+++ other/printDelayed.ts
@@ -1,3 +1,5 @@
+// FIXME: its hard to follow the JS execution logic with so many callbacks and timeouts and async functions
+
 // to simulate the AI typing, we need to print each chunk of the response with a slight delay
 export const printDelayed = (
   text: string,
